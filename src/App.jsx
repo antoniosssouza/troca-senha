1bbb826aa6f4168b4c8f1ab6afe74c6f9cf4e4e2
antoniosssouza <antonio.adolfo89@gmail.com>
2023-06-27 21:18:37 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,42 @@
-import React from 'react'
-import Header from './components/Header/Header'
-import "./App.scss"
-import Tasks from './components/Tasks/Tasks'
-import { db } from './db'
+import { useState } from "react";
 
-const App = () => {
+import logo from "./assets/logo.png";
+
+import "./App.scss";
+
+function App() {
+  const [user, setUser] = useState("");
+
   return (
-    <>
-      <Header />
-      <h1>Otimize seu tempo e se organize com o nosso Planejador Diário.</h1>
-      <Tasks dados={ db } />
-    </>
+    <div className="container">
+      <div className="container-login">
+        <div className="wrap-login">
+          <form className="login-form">
+            <span className="login-form-title">
+              <img src={logo} alt="logo-PMI" />
+            </span>
+
+            <span className="login-form-title"> Troca Senha PMIDOMAIN </span>
+
+
+            <div className="wrap-input">
+              <input
+                className={user !== "" ? "has-val input" : "input"}
+                type="text"
+                value={user}
+                onChange={(e) => setUser(e.target.value)}
+              />
+              <span className="focus-input" data-placeholder="Usuário"></span>
+            </div>
+
+            <div className="container-login-form-btn">
+              <button className="login-form-btn">Troca Senha</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider} from "react-router-dom";
-import "./global.scss";
 import App from './App';
 
 const router = createBrowserRouter([
